e57d63198953c5dba4d93168781455bcabc56498
Franklin Kong <franklinkong981@gmail.com>
2025-01-14 22:49:35 -0800

    Wrote tests for convertToReadableString and addCommas functions

--- addCommas.js
+++ addCommas.js
@@ -1,11 +1,7 @@
 /*Takes in a number and converts it into a string formatted with commas for readability.*/
 function convertToReadableString(number) {
-  //ensures that the input is either an integer or floating point number, handles invalid input format.
-  try {
-    if (isNaN(number)) return "Invalid input type, must be a valid integer or floating point number";
-  } catch(e) {
-    return "Invalid input, must be a valid integer or floating-point number";
-  }
+  //ensures that the input is either an integer or floating point number.
+  if (isNaN(number)) return "Invalid input type, must be a valid integer or floating point number";
 
   return addCommas(number.toString());
 }
@@ -14,8 +10,6 @@ function convertToReadableString(number) {
 function addCommas(numberString) {
   const startIndex = getStartIndex(numberString);
   const endIndex = getEndIndex(numberString);
-  //if non-decimal point is less than 4 digits, no commas are needed.
-  if (endIndex - startIndex <= 3) return numberString;
 
   let formattedNumberString = numberString;
   //start at endIndex and work backwards to startIndex, adding a comma every 3 digits.
@@ -37,4 +31,4 @@ function getEndIndex(numberString) {
   return (decimalIndex === -1) ? numberString.length : decimalIndex;
 }
 
-module.exports = convertToReadableString, addCommas, getStartIndex, getEndIndex;
+module.exports = {convertToReadableString, addCommas, getStartIndex, getEndIndex};
--- addCommas.test.js
+++ addCommas.test.js
@@ -1,7 +1,23 @@
-const addCommas = require("./addCommas");
+const {convertToReadableString, addCommas, getStartIndex, getEndIndex} = require("./addCommas");
 
-describe("#addCommas", () => {
-  test("it is a function", () => {
+const listOfInputs = [500, 1234, 1000000, 9876543210, 6, -10, -5678, 12345.678, -3141592.65];
+const listOfExpectedOutputs = ["500", "1,234", "1,000,000", "9,876,543,210", "6", "-10", "-5,678", "12,345.678", "-3,141,592.65"];
+
+describe("Testing convertToReadableString and addCommas functions", () => {
+  test("convertToReadableString and addCommas are both functions", () => {
+    expect(typeof convertToReadableString).toBe("function");
     expect(typeof addCommas).toBe("function");
   });
+  test("invalid input types are rejected by convertToReadableString", () => {
+    expect(convertToReadableString("abcd")).toBe("Invalid input type, must be a valid integer or floating point number");
+  });
+  test("convertToReadableString and addCommas functions work as intended for valid inputs", () => {
+    for (const [index, input] of listOfInputs.entries()) {
+      expect(convertToReadableString(input)).toBe(listOfExpectedOutputs[index]);
+      expect(addCommas(input.toString())).toBe(listOfExpectedOutputs[index]);
+    }
+  });
 });
+
+
+
