dceefb4cc2c149c65558090229e0f18c8313ed01
Franklin Kong <franklinkong981@gmail.com>
2025-01-14 21:47:16 -0800

    Wrote top-level convert to readable string function

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,16 @@
-function addCommas() {}
+/*Takes in a number and converts it into a string formatted with commas for readability.*/
+function convertToReadableString(number) {
+  //ensures that the input is either an integer or floating point number, handles invalid input format.
+  try {
+    if (isNaN(number)) return "Invalid input type, must be a valid integer or floating point number";
+  } catch(e) {
+    return "Invalid input, must be a valid integer or floating-point number";
+  }
 
-module.exports = addCommas;
+  //If decimal, start at decimal point and work backwards, inserting comma every 3 digits. If whole number, start at end and work backwards.
+  return addCommas(number.toString());
+}
+
+
+
+module.exports = convertToReadableString;
