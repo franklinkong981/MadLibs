100c41b1c05f826250ecdde5ac1ed5a6d604c298
Franklin Kong <franklinkong981@gmail.com>
2025-01-16 11:22:17 -0800

    Created React app and starter code for MadLibs

---
+++ madlibs/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import MadLibs from "./MadLibs.jsx";
+
+function App() {
+
+  return (
+    <>
+      <MadLibs/>
+    </>
+  )
+}
+
+export default App;
