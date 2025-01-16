ca8e370f79a68ebd7532c388dd0259af96e906b1
Franklin Kong <franklinkong981@gmail.com>
2025-01-16 13:57:44 -0800

    Fixed buildMadLibsStory helper function

--- madlibs/src/MadLibs.jsx
+++ madlibs/src/MadLibs.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-import {listOfInputs, restOfStory} from "./MadLibsVariables.js";
+import {buildMadLibsStory} from "./helper.js";
 
 import MadLibsForm from "./MadLibsForm.jsx";
 import MadLibsStory from "./MadLibsStory.jsx";
@@ -8,11 +8,11 @@ import MadLibsStory from "./MadLibsStory.jsx";
 import useToggle from "./hooks/useToggle.jsx";
 
 const MadLibs = () => {
-  const [madLibsValues, setMadLibsValues]= useState({});
+  const [madLibsStory, setMadLibsStory]= useState("");
   const [isFormSubmitted, toggleIsFormSubmitted] = useToggle(false);
 
   const submitMadLibs = (values) => {
-    setMadLibsValues(madLibsValues => values);
+    setMadLibsStory(madLibsStory => buildMadLibsStory(values));
     toggleIsFormSubmitted();
   };
 
@@ -20,7 +20,7 @@ const MadLibs = () => {
     <div className="MadLibs">
       <h1 className="MadLibs-header">MADLIBS!</h1>
       {isFormSubmitted ? (
-        <MadLibsStory values={madLibsValues} restOfStory={restOfStory}/>
+        <MadLibsStory story={madLibsStory} reset={toggleIsFormSubmitted}/>
       ) : (
         <MadLibsForm listOfInputs={listOfInputs} submitMadLibs={submitMadLibs}/>
       )}
--- madlibs/src/helpers.js
+++ madlibs/src/helpers.js
@@ -1,3 +1,5 @@
+import {listOfInputs, restOfStory} from "./MadLibsVariables.js";
+
 function buildInitialFormValues(listOfInputs) {
   const initialFormValues = {};
   for (let input in listOfInputs) {
@@ -6,11 +8,13 @@ function buildInitialFormValues(listOfInputs) {
   return initialFormValues;
 }
 
-function buildMadLibsStory(madLibsValues, restOfStory) {
+function buildMadLibsStory(madLibsValues) {
   let madLibsStory = "";
   for (let i = 0; i < madLibsValues.length; i++) {
-    madLibsStory += restOfStory[i] + madLibsValues[i];
-    if (i == madLibsValues.length - 1) madLibsStory += restOfStory[i + 1];
+    const madLibToInsert = listOfInputs[i].inputName;
+    madLibsStory += restOfStory[i] + madLibsValues[madLibToInsert];
+    
+    if (i == restOfstory.length - 1) madLibsStory += restOfStory[i + 1];
   }
   return madLibsStory;
 }
