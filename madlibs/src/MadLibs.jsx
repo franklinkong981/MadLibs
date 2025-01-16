cac47bcf8ba7d410f4b263ab1160d8839f97d991
Franklin Kong <franklinkong981@gmail.com>
2025-01-16 12:48:10 -0800

    Set up MadLibs component with state and submit form handler

--- madlibs/src/MadLibs.jsx
+++ madlibs/src/MadLibs.jsx
@@ -3,8 +3,10 @@ import React, {useState} from "react";
 import MadLibsForm from "./MadLibsForm.jsx";
 
 const MadLibs = () => {
-  const submitMadLibs = () => {
-    alert("Madlibs submitted!");
+  const [MadLibsValues, setMadLibsValues]= useState({});
+
+  const submitMadLibs = (values) => {
+    setMadLibsValues(values);
   };
 
   return (
