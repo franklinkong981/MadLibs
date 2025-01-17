b941b0e48a0823eb00d798b88e0bec5492c9aecb
Franklin Kong <franklinkong981@gmail.com>
2025-01-17 13:37:28 -0800

    Implemented form validation in the MadLibsForm component

--- madlibs/src/MadLibsForm.jsx
+++ madlibs/src/MadLibsForm.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import {v4 as uuid} from "uuid";
 import {useFormik} from "formik";
 
 import {buildInitialFormValues} from "./helpers.js";
 
 const MadLibsForm = ({listOfInputs, submitMadLibs}) => {
+  const validate = (values) => {
+    const errors = {};
+    for (let value in values) {
+      if (!values[value]) errors[value] = "Required!";
+    }
+    return errors;
+  };
+
   const formik = useFormik({
     initialValues: buildInitialFormValues(listOfInputs),
+    validate,
+    validateOnChange: false,
+    validateOnBlur: false,
     onSubmit: values => {
       submitMadLibs(values);
     }
@@ -20,6 +30,7 @@ const MadLibsForm = ({listOfInputs, submitMadLibs}) => {
           <label htmlFor={`MadLibsForm-${input.inputName}-input`} className="MadLibsForm-label">{input.label}</label>
           <input id={`MadLibsForm-${input.inputName}-input`} className="MadLibsForm-input" type="text" name={input.inputName}
           size="50" value={formik.values[input.inputName]} onChange={formik.handleChange}/>
+          {formik.errors[input.inputName] ? <div className="MadLibsForm-error">{formik.errors[input.inputName]}</div> : null}
         </div>
       ))}
       <button className="MadLibsForm-submit-button" type="submit">Generate MadLibs!</button>
