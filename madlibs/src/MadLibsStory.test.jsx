63834d2be11c98f1565ddc4641e1c0ce6e569fa5
Franklin Kong <franklinkong981@gmail.com>
2025-01-20 20:23:55 -0800

    Wrote successful tests for MadLibsStory

--- madlibs/src/MadLibsStory.test.jsx
+++ madlibs/src/MadLibsStory.test.jsx
@@ -3,3 +3,39 @@
 import React from "react";
 import { render} from "@testing-library/react";
 import MadLibsStory from "./MadLibsStory.jsx";
+import {listOfInputs, restOfStory} from "./MadLibsVariables.js";
+import { expect } from "vitest";
+
+const madLibsValues= {
+  adjective: "fuzzy",
+  "noun-1": "squirrel",
+  color: "blue",
+  "noun-2": "wheel"
+};
+
+//dummy function so all props are defined.
+const restart = () => {
+  console.log("MadLibs restart");
+};
+
+describe("Test <MadLibsStory /> rendering", function () {
+  it("renders without crashing", function() {
+    render(<MadLibsStory values={madLibsValues} listOfInputs={listOfInputs} restOfStory={restOfStory} restart={restart}/>);
+  });
+  it ("matches the snapshot for the MadLibsStory", function() {
+    const {asFragment} = render(<MadLibsStory values={madLibsValues} listOfInputs={listOfInputs} restOfStory={restOfStory} restart={restart}/>);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe("Tests that each component of MadLibsStory renders properly", async function() {
+  it ("Test that the proper MadLibs story text and restart button are rendered.", async function() {
+    const {queryByText} = render(<MadLibsStory values={madLibsValues} listOfInputs={listOfInputs} restOfStory={restOfStory} restart={restart}/>);
+
+    const restartButton = queryByText("Restart");
+    expect(restartButton).toBeInTheDocument();
+    const madLibsStory = queryByText("There was a fuzzy squirrel who loved a blue wheel.");
+    expect(madLibsStory).toBeInTheDocument();
+  });
+});
+
